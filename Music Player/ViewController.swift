1f801bbec6fe46152f089a7e1ffd9a403a27d8c4
eunseochoi <ellenchoi98@outlook.com>
2019-02-17 14:21:35 -0500

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Ellen Choi on 2019-02-17.
+//  Copyright © 2019 Ellen Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
